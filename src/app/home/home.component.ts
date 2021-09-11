a2a04e89dcb9a54e9a7ac10d7155df172d60f39b
Akhlaque <akhlaquea01@gmail.com>
2021-09-12 00:14:34 +0530

    Home Fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,22 +4,22 @@ import Typed from 'typed.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  MyName: string = 'Akhlaque Ahmad';
+  stream:string = 'Developer';
+  constructor() {}
 
   ngOnInit(): void {
     var options = {
-      strings: ['','Full-Stack&nbsp;', 'WEB&nbsp;','Mobile&nbsp; '],
+      strings: ['', 'Full-Stack', 'WEB', 'Mobile'],
       typeSpeed: 120,
       backSpeed: 100,
       loop: true,
     };
 
     var typed = new Typed('.typed', options);
-    typed.reset(true)
+    typed.reset(true);
   }
-
 }
