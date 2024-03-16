7068e5615ab11a27576ae91f2284a82220edb7ef
Akhlaque <akhlaquea01@gmail.com>
2024-03-16 14:26:23 +0530

    Home About section updated

--- src/app/components/main/home/home.component.ts
+++ src/app/components/main/home/home.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  name = "Akhlaque";
+  about = "Passionate Front-end Developer with over 3.2 years of experience in crafting dynamic web applications with a focus on Angular and Node.js.Known for delivering captivating user interfaces and providing technical leadership to enhance team productivity.Proficient in  problem - solving and adept at addressing release challenges with innovative solutions.";
+  hobby = "Outside of the development realm, I'm an avid technology enthusiast, constantly exploring the latest advancements and innovations. I also enjoy unwinding by playing mobile games and spending quality time chilling with friends. Traveling is another passion of mine, as it allows me to explore new cultures and broaden my horizons."
 }
