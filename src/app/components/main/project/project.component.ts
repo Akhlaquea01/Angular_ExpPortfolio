41f33f5d45fca67924afeafd214adce4cc2e1bf6
Akhlaque <akhlaquea01@gmail.com>
2024-03-17 22:24:21 +0530

    project

--- src/app/components/main/project/project.component.ts
+++ src/app/components/main/project/project.component.ts
@@ -4,15 +4,25 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule ],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
   @ViewChild('scrollContainer')
   scrollContainerRef!: ElementRef;
+  repos: any;
+  constructor() {
+    const username = 'Akhlaquea01';
+    const url = `https://api.github.com/users/${username}/repos`;
 
-  constructor() { }
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.repos = data;
+      })
+      .catch(error => console.error('Error fetching profile:', error));
+  }
 
   scrollLeft() {
     this.scrollContainerRef.nativeElement.scrollLeft -= 600; // Adjust this value as needed
